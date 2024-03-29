0cdb867c2143874e997a689f619f58770462ebca
lejlm233 <56920867+lejlm233@users.noreply.github.com>
2024-03-29 22:11:41 +0800

    Update font_definitions.py

--- kivymd/font_definitions.py
+++ kivymd/font_definitions.py
@@ -14,31 +14,31 @@ from kivymd import fonts_path
 
 fonts = [
     {
-        "name": "Roboto",
-        "fn_regular": fonts_path + "OPPOSans20-Regular.ttf",
-        "fn_bold": fonts_path + "OPPOSans20-Bold.ttf",
-        "fn_italic": fonts_path + "OPPOSans20-Regular.ttf",
+        "name": "OPPOSans",
+        "fn_regular": fonts_path + "OPPOSans-Regular.ttf",
+        "fn_bold": fonts_path + "OPPOSans-Bold.ttf",
+        "fn_italic": fonts_path + "OPPOSans-Regular.ttf",
         "fn_bolditalic": fonts_path + "OPPOSans20-Bold.ttf",
     },
     {
-        "name": "RobotoThin",
+        "name": "OPPOSansThin",
         "fn_regular": fonts_path + "OPPOSans20-Light.ttf",
         "fn_italic": fonts_path + "OPPOSans20-Light.ttf",
     },
     {
-        "name": "RobotoLight",
+        "name": "OPPOSansLight",
         "fn_regular": fonts_path + "OPPOSans20-Light.ttf",
         "fn_italic": fonts_path + "OPPOSans20-Light.ttf",
     },
     {
-        "name": "RobotoMedium",
-        "fn_regular": fonts_path + "OPPOSans20-Medium.ttf",
-        "fn_italic": fonts_path + "OPPOSans20-Medium.ttf",
+        "name": "OPPOSansMedium",
+        "fn_regular": fonts_path + "OPPOSans-Medium.ttf",
+        "fn_italic": fonts_path + "OPPOSans-Medium.ttf",
     },
     {
-        "name": "RobotoBlack",
-        "fn_regular": fonts_path + "OPPOSans20-Regular.ttf",
-        "fn_italic": fonts_path + "OPPOSans20-Regular.ttf",
+        "name": "OPPOSansBlack",
+        "fn_regular": fonts_path + "OPPOSans-Regular.ttf",
+        "fn_italic": fonts_path + "OPPOSans-Regular.ttf",
     },
     {
         "name": "Icons",
@@ -61,85 +61,85 @@ theme_font_styles = {
     "Display": {
         "large": {
             "line-height": 1.64,
-            "font-name": "Roboto",
+            "font-name": "OPPOSans",
             "font-size": sp(57),
         },
         "medium": {
             "line-height": 1.52,
-            "font-name": "Roboto",
+            "font-name": "OPPOSans",
             "font-size": sp(45),
         },
         "small": {
             "line-height": 1.44,
-            "font-name": "Roboto",
+            "font-name": "OPPOSans",
             "font-size": sp(36),
         },
     },
     "Headline": {
         "large": {
             "line-height": 1.40,
-            "font-name": "Roboto",
+            "font-name": "OPPOSans",
             "font-size": sp(32),
         },
         "medium": {
             "line-height": 1.36,
-            "font-name": "Roboto",
+            "font-name": "OPPOSans",
             "font-size": sp(28),
         },
         "small": {
             "line-height": 1.32,
-            "font-name": "Roboto",
+            "font-name": "OPPOSans",
             "font-size": sp(24),
         },
     },
     "Title": {
         "large": {
             "line-height": 1.28,
-            "font-name": "Roboto",
+            "font-name": "OPPOSans",
             "font-size": sp(22),
         },
         "medium": {
             "line-height": 1.24,
-            "font-name": "Roboto",
+            "font-name": "OPPOSans",
             "font-size": sp(16),
         },
         "small": {
             "line-height": 1.20,
-            "font-name": "Roboto",
+            "font-name": "OPPOSans",
             "font-size": sp(14),
         },
     },
     "Body": {
         "large": {
             "line-height": 1.24,
-            "font-name": "Roboto",
+            "font-name": "OPPOSans",
             "font-size": sp(16),
         },
         "medium": {
             "line-height": 1.20,
-            "font-name": "Roboto",
+            "font-name": "OPPOSans",
             "font-size": sp(14),
         },
         "small": {
             "line-height": 1.16,
-            "font-name": "Roboto",
+            "font-name": "OPPOSans",
             "font-size": sp(12),
         },
     },
     "Label": {
         "large": {
             "line-height": 1.20,
-            "font-name": "Roboto",
+            "font-name": "OPPOSans",
             "font-size": sp(14),
         },
         "medium": {
             "line-height": 1.16,
-            "font-name": "Roboto",
+            "font-name": "OPPOSans",
             "font-size": sp(12),
         },
         "small": {
             "line-height": 1.16,
-            "font-name": "Roboto",
+            "font-name": "OPPOSans",
             "font-size": sp(11),
         },
     },
